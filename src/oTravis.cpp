d9ef41906dee545efe384996536203d21fbc6518
Schnee Held <schneeheld@mail.ru>
2018-01-13 12:49:20 -0800

    [ci skip]

--- src/oTravis.cpp
+++ src/oTravis.cpp
@@ -12,9 +12,10 @@
 using namespace std;
 
 int main() {
+	
 	cout << "oTravis C++14 project" << endl;
 
-	oTravis::Tria *t = new oTravis::Tria(2,3,4);
+	oTravis::Tria *t = new oTravis::Tria(3,2,1);
 
 	t->show();
 
