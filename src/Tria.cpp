9bae7d199414883d0c31d18d5eacfdf5cee62fe3
Schnee Held <schneeheld@mail.ru>
2018-01-14 19:50:00 -0800

    Update Tria.cpp
    Add DEV output

--- src/Tria.cpp
+++ src/Tria.cpp
@@ -19,6 +19,9 @@ Tria::Tria(size_t s1, size_t s2, size_t s3) : a(s1), b(s2), c(s3) {
 void Tria::show() {
 	std::array<size_t, 3> tuple = { this->a, this->b, this->c };
 	// ranged for loop is supported
+
+	cout << "[DEV]";
+	
 	for (const auto& t : tuple) {
 		std::cout << t << ' ';
 	}
